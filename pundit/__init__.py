581be99b8defb8b85beeacd203b6234c6e68d2bf
Sachin Philip Mathew <me@imsach.in>
2016-05-13 17:35:15 +0530

    white spaces and optimised imports

--- pundit/__init__.py
+++ pundit/__init__.py
@@ -1,5 +1,4 @@
 import itertools
-import json
 
 from pundit.base import PunditBase
 from pundit.condition import ConditionGroup
@@ -14,79 +13,79 @@ inherited features of pundit base and condition groups
 class Pundit(PunditBase, ConditionGroup):
 
 
-	def __init__(self, pundit, conditions):
-		self.pundit = pundit
-		self.condition_val = conditions
-		self.conditions = conditions.conditions
-		self.response = []
-		self.set_input = []
+    def __init__(self, pundit, conditions):
+        self.pundit = pundit
+        self.condition_val = conditions
+        self.conditions = conditions.conditions
+        self.response = []
+        self.set_input = []
 
 
-	def evaluate(self, arg1, arg2 = None):
-		arg_one = arg1.lower() if type(arg1) == str else arg1
-		arg_two = arg2.lower() if type(arg2) == str else arg2
+    def evaluate(self, arg1, arg2 = None):
+        arg_one = arg1.lower() if type(arg1) == str else arg1
+        arg_two = arg2.lower() if type(arg2) == str else arg2
 
         #appending to a sample dump with the struture
-		self.set_input = []
-		self.response = []
-		self.set_input.append({self.pundit.arg1: arg_one, self.pundit.arg2:arg_two})
+        self.set_input = []
+        self.response = []
+        self.set_input.append({self.pundit.arg1: arg_one, self.pundit.arg2:arg_two})
 
         # checking for the conditions
-		for yme in self.conditions:
+        for yme in self.conditions:
 
-			if yme['optn'] == 'IN':
-				self.response.append(True) if True in [yme['l1'] in  self.set_input[0][yme['arg']]] else self.response.append(False)
-			elif yme['optn'] == 'NOT_IN':
-				self.response.append(False) if True in [yme['l1'] in self.set_input[0][yme['arg']]] else self.response.append(True)
-			elif yme['optn'] == 'NOT_IS':
-				pass
-			elif yme['optn'] == 'EQ':
-				self.response.append(True) if True in [yme['l1'] == self.set_input[0][yme['arg']]] else self.response.append(False)
-			elif yme['optn'] == 'NOT_EQ':
-				self.response.append(True) if True in [yme['l1'] !=  self.set_input[0][yme['arg']]] else self.response.append(False)
-			elif yme['optn'] == 'GT':
-				self.response.append(True) if True in [yme['l1'] < self.set_input[0][yme['arg']]] else self.response.append(False)
-			elif yme['optn'] == 'LT':
-				self.response.append(True) if True in [yme['l1'] > self.set_input[0][yme['arg']]] else self.response.append(False)
-			elif yme['optn'] == 'GTE':
-				self.response.append(True) if True in [yme['l1'] <=  self.set_input[0][yme['arg']]] else self.response.append(False)
-			elif yme['optn'] == 'LTE':
-				self.response.append(True) if True in [yme['l1'] >=  self.set_input[0][yme['arg']]] else self.response.append(False)
-			else:
-				return "unsupported operation"
+            if yme['optn'] == 'IN':
+                self.response.append(True) if True in [yme['l1'] in  self.set_input[0][yme['arg']]] else self.response.append(False)
+            elif yme['optn'] == 'NOT_IN':
+                self.response.append(False) if True in [yme['l1'] in self.set_input[0][yme['arg']]] else self.response.append(True)
+            elif yme['optn'] == 'NOT_IS':
+                pass
+            elif yme['optn'] == 'EQ':
+                self.response.append(True) if True in [yme['l1'] == self.set_input[0][yme['arg']]] else self.response.append(False)
+            elif yme['optn'] == 'NOT_EQ':
+                self.response.append(True) if True in [yme['l1'] !=  self.set_input[0][yme['arg']]] else self.response.append(False)
+            elif yme['optn'] == 'GT':
+                self.response.append(True) if True in [yme['l1'] < self.set_input[0][yme['arg']]] else self.response.append(False)
+            elif yme['optn'] == 'LT':
+                self.response.append(True) if True in [yme['l1'] > self.set_input[0][yme['arg']]] else self.response.append(False)
+            elif yme['optn'] == 'GTE':
+                self.response.append(True) if True in [yme['l1'] <=  self.set_input[0][yme['arg']]] else self.response.append(False)
+            elif yme['optn'] == 'LTE':
+                self.response.append(True) if True in [yme['l1'] >=  self.set_input[0][yme['arg']]] else self.response.append(False)
+            else:
+                return "unsupported operation"
 
-		return self.condition_val.success if True in self.response else self.condition_val.failed
+        return self.condition_val.success if True in self.response else self.condition_val.failed
 
 
 
 class MathRuler():
-	"""Mathemaical rule operatons"""
+    """Mathemaical rule operatons"""
 
-	def __init__(self, for_this, then_that):
-		self.condition = for_this;
-		self.then = then_that
+    def __init__(self, for_this, then_that):
+        self.condition = for_this;
+        self.then = then_that
 
 
-	def math(self, con):
-		then_that = self.then
-		for_this = self.condition
-		exp_converter = then_that.replace(for_this, str(con))
-		return eval(exp_converter)
+    def math(self, con):
+        then_that = self.then
+        for_this = self.condition
+        exp_converter = then_that.replace(for_this, str(con))
+        return eval(exp_converter)
 
 
 
 class ListRuler():
 
-	def __init__(self, arg_list):
-		self.list = arg_list
-		self.reduced = []
-		for li in self.list:
-			if type(li) == list:
-				for cc in list(itertools.chain(li)):
-					self.reduced.append(cc)
-			else:
-				self.reduced.append(li)
+    def __init__(self, arg_list):
+        self.list = arg_list
+        self.reduced = []
+        for li in self.list:
+            if type(li) == list:
+                for cc in list(itertools.chain(li)):
+                    self.reduced.append(cc)
+            else:
+                self.reduced.append(li)
 
 
-	def fullfill(self, arg):
-		return True if arg in self.reduced else False
+    def fullfill(self, arg):
+        return True if arg in self.reduced else False
--- pundit/condition.py
+++ pundit/condition.py
@@ -1,27 +1,22 @@
-import json
-
-
 '''
 Condition group where conditions are set
 '''
 
+
 class ConditionGroup():
+    def __init__(self):
+        self.conditions = []
+        self.success = 'success'
+        self.failed = 'failed'
 
-	def __init__(self):
-		self.conditions = []
-		self.success = 'success'
-		self.failed = 'failed'
+    def add_condition(self, l1, optn, arg, then, else_value):
+        input_json = {'l1': l1, 'optn': optn, 'arg': arg, 'then': then, 'else_value': else_value}
+        self.conditions.append(input_json)
 
+    @property
+    def conditions(self):
+        return self.conditions
 
-	def add_condition(self, l1, optn, arg, then, else_value):
-		input_json = {'l1': l1, 'optn':optn, 'arg':arg, 'then': then, 'else_value':else_value}
-		self.conditions.append(input_json)
-
-	@property
-	def conditions(self):
-	    return self.conditions
-
-
-	def response(self, success, failed):
-		self.success = success
-		self.failed = failed
+    def response(self, success, failed):
+        self.success = success
+        self.failed = failed
